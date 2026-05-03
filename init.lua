39e021a29a863f096a21fb0eb3928dec9fdb2a16
Gustafssoon <gabrielgustafsson96@gmail.com>
2026-05-03 17:11:16 +0200

    chore(config): rebrand client for GabalexOT

--- init.lua
+++ init.lua
@@ -1,35 +1,37 @@
 -- CONFIG
-APP_NAME = "otclientv8"  -- important, change it, it's name for config dir and files in appdata
-APP_VERSION = 1341       -- client version for updater and login to identify outdated client
-DEFAULT_LAYOUT = "retro" -- on android it's forced to "mobile", check code bellow
+APP_NAME = "gabalexot"  -- config dir/files in appdata
+APP_VERSION = 1341
+DEFAULT_LAYOUT = "retro"
 
--- If you don't use updater or other service, set it to updater = ""
+-- Disable default OTCv8 online services unless you use your own updater/crash/status backend
 Services = {
-  website = "http://otclient.ovh", -- currently not used
-  updater = "http://otclient.ovh/api/updater.php",
+  website = "",
+  updater = "",
   stats = "",
-  crash = "http://otclient.ovh/api/crash.php",
-  feedback = "http://otclient.ovh/api/feedback.php",
-  status = "http://otclient.ovh/api/status.php"
+  crash = "",
+  feedback = "",
+  status = ""
 }
 
 -- Servers accept http login url, websocket login url or ip:port:version
+-- First entry: NAS/LAN
 Servers = {
---[[  OTClientV8 = "http://otclient.ovh/api/login.php",
-  OTClientV8proxy = "http://otclient.ovh/api/login.php?proxy=1",
-  OTClientV8c = "otclient.ovh:7171:1099:25:30:80:90",
-  OTClientV8Test = "http://otclient.ovh/api/login2.php",
-  Evoulinia = "evolunia.net:7171:1098",
-  GarneraTest = "garnera-global.net:7171:1100",
-  LocalTestServ = "127.0.0.1:7171:1098:110:30:93"  ]]
+  -- GabalexOT på NAS / lokalt nätverk
+  GabalexOT = "192.168.50.80:7171:860",
+
+  -- GabalexOT via publik IP
+  GabalexOnline = "83.227.227.98:7171:860",
+
+  -- Bara om servern körs på samma dator som klienten
+  GabalexLocal = "127.0.0.1:7171:860",
 }
 
---Server = "ws://otclient.ovh:3000/"
 --Server = "ws://127.0.0.1:88/"
---USE_NEW_ENERGAME = true -- uses entergamev2 based on websockets instead of entergame
-ALLOW_CUSTOM_SERVERS = true -- if true it shows option ANOTHER on server list
+--USE_NEW_ENERGAME = true
 
-g_app.setName("OTCv8")
+ALLOW_CUSTOM_SERVERS = true
+
+g_app.setName("GabalexOT")
 -- CONFIG END
 
 -- print first terminal message
